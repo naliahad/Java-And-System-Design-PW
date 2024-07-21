c142d15c9c4da07a6f3729df2f6842d75e651b84
naliahad <ahadali2842000@gmail.com>
2024-07-21 16:26:05 +0530

    Io-File creation

--- Module50/IO/CreateFileDir.java
+++ Module50/IO/CreateFileDir.java
@@ -4,16 +4,16 @@ public class CreateFileDir
 {
     public static void main(String[] args) throws IOException
     {
-        // File file1 = new File("pw.txt");
-        // System.out.println(file1.exists());
+        // File file1 = new File("pw.txt"); // If ow.txt already on system jvm will start refering to it else it will createa java object
+        // System.out.println(file1.exists());// To check if file exist or not: true/false
 
-        //     file1.createNewFile();
+        //     file1.createNewFile(); //To create file -> throws exception.
         //     System.out.println(file1.exists());
 
         File dir  = new File("PW");
         System.out.println(dir.exists());
 
-        dir.mkdir();
+        dir.mkdir(); // To create directory
         System.out.println(dir.exists());
     }
 }
