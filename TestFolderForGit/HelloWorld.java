ab483ca339683d4f65a9d28f93677e3dcfac5bf6
naliahad <ahadali2842000@gmail.com>
2024-08-04 20:09:34 +0530

    git command, rdbms connected in test module

--- TestFolderForGit/HelloWorld.java
+++ TestFolderForGit/HelloWorld.java
@@ -3,6 +3,29 @@ package TestFolderForGit;
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
+
+/* ls -a
+git remote -v
+git remote rm origin
+git remote -v
+sign in github account ->  Create Repo -> public -> create
+git status
+git config user.name naliahad
+git config user.email ahadali2842000@gmail.com
+git add .
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/naliahad/Java-Script-.git
+git push -u origin main
+*/
+
+/* or push an existing repository from the command line
+git remote add origin https://github.com/naliahad/Java-Script-.git
+git branch -M main
+git push -u origin main
+*/
     }
 }
 
---
+++ TestModule/jdbcTest.java
@@ -0,0 +1,9 @@
+package TestModule;
+
+public class jdbcTest 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World");    
+    }
+}
