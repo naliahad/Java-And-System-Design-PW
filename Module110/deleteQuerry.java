522005a02a92852a537dbf70c488b51656bf3c2c
naliahad <ahadali2842000@gmail.com>
2024-08-05 17:51:27 +0530

    Code to execute DeleteQuery

---
+++ Module110/deleteQuerry.java
@@ -0,0 +1,48 @@
+package Module110;
+
+import java.sql.Statement; 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+public class deleteQuerry 
+{
+    public static void main(String[] args) throws ClassNotFoundException, SQLException
+    {
+
+        //Code to execute DeleteQuery
+
+        //load and register the driver
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        //Establish the Connection
+        String url = "jdbc:mysql://localhost:3306/pwskillsbatch";
+        String user = "root";
+        String password = "targetatlas@1234";
+        Connection connection = DriverManager.getConnection(url, user, password);
+    
+        //Create a Statement Object
+        Statement statement = connection.createStatement();
+
+        // Execute the query
+        String sqlDeleteQuery = "delete from student where sid =18"; 
+        int rowAffected =statement.executeUpdate(sqlDeleteQuery);
+
+        //prosess the result
+        if(rowAffected == 1)
+        {
+            System.out.println("No of rows affected is : " + rowAffected);
+        }
+        else
+        {
+            System.out.println("No record found for deletion");
+        }
+
+
+        //close the resources
+        
+        statement.close();
+        connection.close();
+       
+    }
+}
