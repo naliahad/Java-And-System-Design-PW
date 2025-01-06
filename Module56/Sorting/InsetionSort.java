0d11b3ff0333aa0f7e286643ee8d0807d79e23f7
naliahad <ahadali2842000@gmail.com>
2025-01-07 01:07:04 +0530

    Insertion Sort

--- Module56/Sorting/InsetionSort.java
+++ Module56/Sorting/InsetionSort.java
@@ -1,9 +1,29 @@
 package Module56.Sorting;
 
+import java.util.Arrays;
+
 public class InsetionSort 
 {
+    public static void insertionSort(int []arr)
+    {
+        for(int i = 0; i<arr.length; i++)
+        {
+            int j =i;
+            while(j>0 && arr[j] < arr[j-1])
+            {
+                //swap
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+
+                j--;
+            }
+        }
+    }
     public static void main(String[] args) 
     {
-        
+        int arr[] = {20,50,30,90,5,15};
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
