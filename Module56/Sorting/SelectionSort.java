a1847d014a7f7fe580b1ca87124c3a55c2cffec3
naliahad <ahadali2842000@gmail.com>
2025-01-07 01:47:50 +0530

    Selection Sort

---
+++ Module56/Sorting/SelectionSort.java
@@ -0,0 +1,16 @@
+package Module56.Sorting;
+
+public class SelectionSort 
+{
+    private static void selectionSort(int[] arr) {
+        // TODO Auto-generated method stub
+        int min_idx = 0;
+
+    }
+    public static void main(String[] args) 
+    {
+        int arr[] = 
+        selectionSort(arr);
+    }
+       
+}
