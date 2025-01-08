b3c59ebc375f65461b1d7903ce83c159a25398e7
naliahad <ahadali2842000@gmail.com>
2025-01-08 10:58:45 +0530

    Insertion Sort

--- Module56/Sorting/SelectionSort.java
+++ Module56/Sorting/SelectionSort.java
@@ -1,16 +1,41 @@
 package Module56.Sorting;
 
+import java.util.*;
+
 public class SelectionSort 
 {
-    private static void selectionSort(int[] arr) {
+    public static void selectionSort(int[] arr) {
         // TODO Auto-generated method stub
-        int min_idx = 0;
+        for(int i=0; i<arr.length; i++)
+        {
+        int min_idx = i; // value of i  mimnimum value in every iteration
+            for(int j=i+1; j<arr.length; j++)
+            {
+                if(arr[j]< arr[min_idx])
+                {
+                    min_idx = j;
+                }
 
+                if(min_idx != i)
+                {
+                    int temp = arr[min_idx];
+                    arr[min_idx] = arr[i];
+                    arr[i] = temp;
+
+                }
+            }
+        }
+        
     }
     public static void main(String[] args) 
     {
-        int arr[] = 
+        /*
+            1. At every iteration, smallest will be at the left.
+            2. At every itereation only one swap is required.      
+        */
+        int arr[] = {20,13,23,1,5,7};
         selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
        
 }
