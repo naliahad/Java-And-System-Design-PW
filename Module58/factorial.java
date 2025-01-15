a371b15c99dda2d14ab109c0228449b6b88336e0
naliahad <ahadali2842000@gmail.com>
2025-01-15 16:04:07 +0530

    Factorial

--- Module58/factorial.java
+++ Module58/factorial.java
@@ -1,13 +1,22 @@
 package Module58;
 
 class factorial{
-    public static int f(int n){
-        if (n == 0) return 1;
-        int smallAns = f(n-1);
-        return n*smallAns;
+    public static int f(int n)
+    {
+        int result = 0;
+        if (n == 0) 
+        {
+            return 1;
+        }
+        else
+        {
+            result = n*f(n-1);
+        }
+       
+        return result;
     }
     public static void main(String[] args) {
-        int n = 4;
+        int n = 5;
         System.out.println(f(n));
         
     }
